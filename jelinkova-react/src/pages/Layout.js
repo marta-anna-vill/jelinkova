957c42c7e547dbe227c6c96b050306a4bc0bc601
marta-anna-vill <marta.halamkova@puxdesign.cz>
2022-05-26 22:34:35 +0200

    submenu

--- jelinkova-react/src/pages/Layout.js
+++ jelinkova-react/src/pages/Layout.js
@@ -21,7 +21,7 @@ const Layout = () => {
     };
 
     useEffect(() => {
-        window.addEventListener("scroll", function() {
+        window.addEventListener("scroll", function () {
             handleScroll();
         });
     });
@@ -50,7 +50,11 @@ const Layout = () => {
                                     <ul>
                                         <li>
                                             <Link to="/massages/masaz-1" >Masáž 1</Link>
+                                        </li>
+                                        <li>
                                             <Link to="/massages/masaz-2" >Masáž 2</Link>
+                                        </li>
+                                        <li>
                                             <Link to="/massages/masaz-3" >Masáž 3</Link>
                                         </li>
                                     </ul>
@@ -81,20 +85,20 @@ const Layout = () => {
                                 <h2 className="footer-title">Kde mě najdete</h2>
                                 Křenová 69, 602 00 Brno
                                 <div className="footer-map">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.4817992526164!2d16.62284201591142!3d49.1914190851534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471294457313f187%3A0x7dc23d5d63eeae3b!2sPUXdesign%20%E2%80%94%20Weby%2C%20e-shopy%20a%20port%C3%A1ly%20na%20m%C3%ADru!5e0!3m2!1scs!2scz!4v1653338123133!5m2!1scs!2scz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.4817992526164!2d16.62284201591142!3d49.1914190851534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471294457313f187%3A0x7dc23d5d63eeae3b!2sPUXdesign%20%E2%80%94%20Weby%2C%20e-shopy%20a%20port%C3%A1ly%20na%20m%C3%ADru!5e0!3m2!1scs!2scz!4v1653338123133!5m2!1scs!2scz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                                 <h2 className="footer-title">Sledujte mě</h2>
                                 <div className="social-container">
                                     <a className="social-item"
                                         href="https://nicepage.com/templates/preview/exceptional-beauty-service-1292320?device=desktop"
                                         target="_blank" rel="noreferrer">
-                                        <img src={iconFacebook} alt=""/>
-                                    
+                                        <img src={iconFacebook} alt="" />
+
                                     </a>
                                     <a className="social-item"
                                         href="https://nicepage.com/templates/preview/exceptional-beauty-service-1292320?device=desktop"
-                                        target="_blank"  rel="noreferrer">
-                                        <img src={iconInstagram} alt=""/>
+                                        target="_blank" rel="noreferrer">
+                                        <img src={iconInstagram} alt="" />
                                     </a>
                                 </div>
                                 <h2 className="footer-title">Ceritikace</h2>
@@ -120,16 +124,16 @@ const Layout = () => {
                                 <h3>Služby</h3>
                                 <ul>
                                     <li>
-                                    <Link to="/">Masáž 1</Link>
+                                        <Link to="/">Masáž 1</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 2</Link>
+                                        <Link to="/">Masáž 2</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 3</Link>
+                                        <Link to="/">Masáž 3</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 4</Link>
+                                        <Link to="/">Masáž 4</Link>
                                     </li>
                                 </ul>
                             </div>
@@ -137,21 +141,21 @@ const Layout = () => {
                                 <h3>Užitečné odkazy</h3>
                                 <ul>
                                     <li>
-                                    <Link to="/">Masáž 1</Link>
+                                        <Link to="/">Masáž 1</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 2</Link>
+                                        <Link to="/">Masáž 2</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 3</Link>
+                                        <Link to="/">Masáž 3</Link>
                                     </li>
                                     <li>
-                                    <Link to="/">Masáž 4</Link>
+                                        <Link to="/">Masáž 4</Link>
                                     </li>
                                 </ul>
                             </div>
                             <div className="footer-middle-col">
-                            
+
                             </div>
                         </div>
                     </div>
